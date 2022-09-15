f1330f6606ae133b5be21a05439af69a2c0d119a
David <davidkostic99@gmail.com>
2022-09-15 21:50:46 +0200

    ima component za register i routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { InputComponent } from './components/input/input.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
@@ -11,9 +13,11 @@ import { InputComponent } from './components/input/input.component';
     AppComponent,
     LoginComponent,
     InputComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
