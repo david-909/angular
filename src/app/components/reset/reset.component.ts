ea01e1c9c0b9a1db908643e4497c617a197fa54b
David <davidkostic99@gmail.com>
2022-10-06 15:31:06 +0200

    jos jedan toastr

--- src/app/components/reset/reset.component.ts
+++ src/app/components/reset/reset.component.ts
@@ -1,5 +1,6 @@
+import { ToastrService } from 'ngx-toastr';
 import { ResetPasswordService } from './../../services/reset-password.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,7 +12,9 @@ import { Component, OnInit } from '@angular/core';
 export class ResetComponent implements OnInit {
   form: FormGroup
   token = '';
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private resetPasswordService: ResetPasswordService) { }
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private resetPasswordService: ResetPasswordService,
+              private toastr: ToastrService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: any)=>{
@@ -27,8 +30,18 @@ export class ResetComponent implements OnInit {
       password: this.form.value.password,
       token: this.token
     }
-    this.resetPasswordService.reset(data).subscribe((res)=>{
-      console.log(res);
+    this.resetPasswordService.reset(data).subscribe({
+      next: (res: any)=>{
+        this.toastr.success(res, "Uspesno")
+        this.router.navigate(["/login"]);
+      },
+      error: (err: any)=>{
+        let greska = err.error.split("ERROR: ");
+        delete greska[Object.keys(greska)[0]];;
+        greska.forEach((e: any) => {
+          this.toastr.error(e, "Greska");
+        });
+      }
     })
   }
 
